52d4e19df51cbbee0f55a5907c2821d617221aae
Olakusibe <125964202+Ola-code55@users.noreply.github.com>
2023-08-16 00:11:35 +0100

    added the styling to my project

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Weather.css";
+import './Weather.css';
 export default function Weather() {
     return (
         <div className="Weather">
@@ -10,10 +10,11 @@ export default function Weather() {
                 type="search"
                 placeholder="Enter a city.."
                 className="form-control search-input"
+                autoFocus="on"
                 onChange={""}
               />
             </div>
-            <div className="col-3 p-0">
+            <div className="col-3 ">
               <input
                 type="submit"
                 value="Search"
@@ -22,11 +23,34 @@ export default function Weather() {
             </div>
           </div>
         </form>
+
          <h1>Lagos</h1>      
    <ul>
     <li>Tuesday 14:00</li>
     <li>Cloudy</li>
- </ul>     
-        </div>
+ </ul>  
+
+
+ <div className="row mt-3">
+    <div className="col-6">
+      <div className="clearfix">
+        <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy" 
+           className="float-left"
+        />
+<div className="float-left">
+<span className="temperature">27</span>
+    <span className="unit">℃</span>
+    </div>  
+    </div>
+    </div> 
+    <div className = "col-6">
+        <ul>
+            <li> Precipitation: 71%</li>
+            <li>Wind: 13 km/h</li>
+        
+        </ul>
+    </div>
+    </div>  
+     </div>   
     )
 }
