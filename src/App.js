cffb75b1422c0cc398c5ffe9ddd25f05eb97a4f8
Olakusibe <125964202+Ola-code55@users.noreply.github.com>
2023-08-15 20:52:03 +0100

    created a footer and form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import Weather from "./Weather"
 import './App.css';
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-  <Weather/>
-  </div>
+    <Weather/>
+      <footer>
+        This project was coded by Olakusibe Animashaun and it is {""}
+        <a href="https://github.com/Ola-code55/react-weather-search-engine" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+      </div>
     </div>
-  
   );
 }
 
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,32 @@
 import React from "react";
-
+import "./Weather.css";
 export default function Weather() {
     return (
         <div className="Weather">
-            <h1>Weather in Lagos</h1>        
+             <form onSubmit={""}>
+          <div className="row">
+            <div className="col-9 ">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control search-input"
+                onChange={""}
+              />
+            </div>
+            <div className="col-3 p-0">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+         <h1>Lagos</h1>      
    <ul>
     <li>Tuesday 14:00</li>
     <li>Cloudy</li>
- </ul>
-            
+ </ul>     
         </div>
     )
 }
