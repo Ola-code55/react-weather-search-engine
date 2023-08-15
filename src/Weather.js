def959cac8b846365803f5e9b923c93deb0798dc
Olakusibe <125964202+Ola-code55@users.noreply.github.com>
2023-08-15 19:37:48 +0100

    removed unwanted files

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Weather() {
+    return (
+        <div className="Weather">
+            <h1>Weather in Lagos</h1>        
+   <ul>
+    <li>Tuesday 14:00</li>
+    <li>Cloudy</li>
+ </ul>
+            
+        </div>
+    )
+}
